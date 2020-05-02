892f867398ce1b11ffbe47873deb0ba1748fe7d1
Diego Medeiros <djbmbb@hotmail.com>
2020-05-02 11:53:54 -0300

    testes wip

--- src/main/java/com/example/scconfig/ScconfigApplication.java
+++ src/main/java/com/example/scconfig/ScconfigApplication.java
@@ -2,8 +2,10 @@ package com.example.scconfig;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class ScconfigApplication {
 
     public static void main(String[] args) {
