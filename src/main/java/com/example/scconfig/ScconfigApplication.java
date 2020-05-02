fe4c17d34cbd2a2fa8271d97a7406975928a19c5
Diego Medeiros <djbmbb@hotmail.com>
2020-05-02 08:37:32 -0300

    init

---
+++ src/main/java/com/example/scconfig/ScconfigApplication.java
@@ -0,0 +1,13 @@
+package com.example.scconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScconfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScconfigApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/scconfig/ScconfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.scconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScconfigApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
