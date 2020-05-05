9cf1be014c21b9d1489785845356cad81085fe29
Diego Medeiros <djbmbb@hotmail.com>
2020-05-04 22:03:38 -0300

    commit dev

--- src/main/java/com/example/scconfig/ScconfigApplication.java
+++ src/main/java/com/example/scconfig/ScconfigApplication.java
@@ -1,9 +1,11 @@
 package com.example.scconfig;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
+@Slf4j
 @SpringBootApplication
 @EnableConfigServer
 public class ScconfigApplication {
